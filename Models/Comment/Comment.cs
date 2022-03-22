f83c2f96af2c0634925311a01faa7a113fdd3d37
Artem Krasov <artkrasov@gmail.com>
2022-03-22 14:46:18 +0300

    Fixed bugs with a comment

--- Models/Comment/Comment.cs
+++ Models/Comment/Comment.cs
@@ -23,7 +23,6 @@ namespace Flowchart_Editor.Models.Comment
         public Line fourthLine;
         public Line fifthLine;
         public Line sixthtLine;
-        public Line seventhLine;
 
         public UIElement GetUIElementWithoutCreate() => canvasOComment;
 
@@ -54,68 +53,70 @@ namespace Flowchart_Editor.Models.Comment
                 fourthLine = new Line();
                 fifthLine = new Line();
                 sixthtLine = new Line();
-                seventhLine = new Line();
 
                 BrushConverter color = new BrushConverter();
-
+             
                 firstLine.X1 = 30;
-                firstLine.Y1 = 10;
+                firstLine.Y1 = 40;
                 firstLine.X2 = 50;
-                firstLine.Y2 = 10;
+                firstLine.Y2 = 40;
                 firstLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 secondLine.X1 = 60;
-                secondLine.Y1 = 10;
+                secondLine.Y1 = 40;
                 secondLine.X2 = 80;
-                secondLine.Y2 = 10;
+                secondLine.Y2 = 40;
                 secondLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 thirdLine.X1 = 90;
-                thirdLine.Y1 = 10;
+                thirdLine.Y1 = 40;
                 thirdLine.X2 = 110;
-                thirdLine.Y2 = 10;
+                thirdLine.Y2 = 40;
                 thirdLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 fourthLine.X1 = 110;
-                fourthLine.Y1 = 40;
+                fourthLine.Y1 = 0;
                 fourthLine.X2 = 110;
-                fourthLine.Y2 = -20;
+                fourthLine.Y2 = 75;
                 fourthLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 fifthLine.X1 = 120;
-                fifthLine.Y1 = -20;
+                fifthLine.Y1 = 0;
                 fifthLine.X2 = 110;
-                fifthLine.Y2 = -20;
+                fifthLine.Y2 = 0;
                 fifthLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 sixthtLine.X1 = 120;
-                sixthtLine.Y1 = 40;
+                sixthtLine.Y1 = 75;
                 sixthtLine.X2 = 110;
-                sixthtLine.Y2 = 40;
+                sixthtLine.Y2 = 75;
                 sixthtLine.Stroke = (Brush)color.ConvertFrom(defaulLineColor);
 
                 textBoxOfComment.Text = textOfActionBlock;
                 textBoxOfComment.Width = 150;
-                textBoxOfComment.Height = 250;
+                textBoxOfComment.Height = 75;
                 textBoxOfComment.FontSize = defaulFontSize;
                 textBoxOfComment.FontFamily = defaultFontFamily;
                 textBoxOfComment.VerticalAlignment = VerticalAlignment.Center;
                 textBoxOfComment.HorizontalAlignment = HorizontalAlignment.Center;
                 textBoxOfComment.TextAlignment = TextAlignment.Center;
-                textBoxOfComment.Foreground = Brushes.White;
+                textBoxOfComment.Foreground = Brushes.Black;
                 textBoxOfComment.TextWrapping = TextWrapping.Wrap;
                 textBoxOfComment.MouseDoubleClick += ChangeTextBoxToLabel;
+                Canvas.SetLeft(textBoxOfComment, 110);
+
 
                 textBlockOfComment.Width = 150;
-                textBlockOfComment.Height = 250;
+                textBlockOfComment.Height = 750;
                 textBlockOfComment.FontSize = defaulFontSize;
                 textBlockOfComment.FontFamily = defaultFontFamily;
                 textBlockOfComment.VerticalAlignment = VerticalAlignment.Center;
                 textBlockOfComment.HorizontalAlignment = HorizontalAlignment.Center;
                 textBlockOfComment.TextAlignment = TextAlignment.Center;
-                textBlockOfComment.Foreground = Brushes.White;
+                textBlockOfComment.Foreground = Brushes.Black;
                 textBlockOfComment.TextWrapping = TextWrapping.Wrap;
                 textBlockOfComment.MouseDown += ChangeTextBoxToLabel;
+                Canvas.SetLeft(textBoxOfComment, 110);
 
                 canvasOComment.Children.Add(firstLine);
                 canvasOComment.Children.Add(secondLine);
@@ -123,8 +124,7 @@ namespace Flowchart_Editor.Models.Comment
                 canvasOComment.Children.Add(fourthLine);
                 canvasOComment.Children.Add(fifthLine);
                 canvasOComment.Children.Add(sixthtLine);
-                canvasOComment.Children.Add(seventhLine);
-
+                canvasOComment.Children.Add(textBoxOfComment);
                 canvasOComment.MouseMove += actionBlockForMovements_MouseMove;
             }
             return canvasOComment;
@@ -139,6 +139,7 @@ namespace Flowchart_Editor.Models.Comment
                     canvasOComment.Children.Remove(textBoxOfComment);
                     canvasOComment.Children.Remove(textBlockOfComment);
                     textBoxOfComment.Text = textBlockOfComment.Text;
+                    Canvas.SetLeft(textBoxOfComment, 110);
                     canvasOComment.Children.Add(textBoxOfComment);
                     textChangeStatus = false;
                     valueOfClicksOnTextBlock = 0;
@@ -150,6 +151,7 @@ namespace Flowchart_Editor.Models.Comment
                 canvasOComment.Children.Remove(textBlockOfComment);
                 textBlockOfComment.Text = textBoxOfComment.Text;
                 Canvas.SetTop(textBlockOfComment, 3.5);
+                Canvas.SetLeft(textBlockOfComment, 110);
                 canvasOComment.Children.Add(textBlockOfComment);
                 textChangeStatus = true;
             }
