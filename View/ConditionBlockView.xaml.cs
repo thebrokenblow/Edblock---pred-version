48d34720250861bbe41d2f245e90589c21d62560
Artem <artkrasov@gmail.com>
2022-05-15 12:40:49 +0300

    Added Blocks to UserControl

---
+++ View/ConditionBlockView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flowchart_Editor.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ConditionBlockView.xaml
+    /// </summary>
+    public partial class ConditionBlockView : UserControl
+    {
+        public ConditionBlockView()
+        {
+            InitializeComponent();
+        }
+    }
+}
