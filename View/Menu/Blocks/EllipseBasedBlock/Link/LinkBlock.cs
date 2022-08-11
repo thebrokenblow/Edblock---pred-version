5cd2f2fefb489811240b66b849398ecc77175fef
Artem <artkrasov@gmail.com>
2022-08-11 19:09:38 +0300

    Identified a lot of blocks that use Ellipse as a basis and implemented common functionality for them using the interface (eliminated code duplication)

---
+++ View/Menu/Blocks/EllipseBasedBlock/Link/LinkBlock.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using Flowchart_Editor.Model;
+using Flowchart_Editor.View.Menu.Blocks.EllipseBasedBlock;
+
+namespace Flowchart_Editor.Models
+{
+    [BlockName("LinkBlock")]
+    public class LinkBlock : Block, IEllipseBased
+    {
+        private const int valueOffsetTextField = 10;
+        public LinkBlock(Canvas destination)
+        {
+            EditField = destination;
+            initialText = "Ссылка";
+
+            SetPropertyEllipseBlock();
+            ControlSize sizeTextField = GetSizeTextField(ControlSize);
+            ControlOffset offsetTextField = GetOffsetTextField();
+            SetPropertyTextField(sizeTextField, offsetTextField);
+
+            SetCoordinatesConnectionPoints(offsetConnectionPoint);
+            InitializingConnectionPoints(listCoordinatesConnectionPoints);
+        }
+
+        override public UIElement GetUIElement()
+        {
+            return FrameBlock;
+        }
+
+        private static ControlSize GetSizeTextField(ControlSize controlSize)
+        {
+            double width = controlSize.Height - valueOffsetTextField * 2;
+            double height = controlSize.Height - valueOffsetTextField * 2;
+            ControlSize sizeTextField = new(width, height);
+            return sizeTextField;
+        }
+
+        private static ControlOffset GetOffsetTextField()
+        {
+            double offsetLeft = valueOffsetTextField;
+            double offsetTop = valueOffsetTextField;
+            ControlOffset offsetTextField = new(offsetLeft, offsetTop);
+            return offsetTextField;
+        }
+
+        public override void SetWidth(int valueBlockWidth)
+        {
+
+        }
+
+        public override void SetHeight(int valueBlockHeight)
+        {
+
+        }
+
+        public void SetPropertyEllipseBlock()
+        {
+            ControlSize.Width = ControlSize.Height;
+            SetPropertyFrameBlock();
+            Ellipse linkBlock = new();
+
+            string color = "#FF5761A8";
+            Brush backgroundColor = GetBackgroundColor(color);
+            IEllipseBased.SetBackground(linkBlock, backgroundColor);
+            IEllipseBased.SetSize(linkBlock, ControlSize);
+            IEllipseBased.AddEllipse(FrameBlock, linkBlock);
+        }
+    }
+}
---
+++ View/Menu/Blocks/EllipseBasedBlock/Link/LinkBlockView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Flowchart_Editor.Models;
+using System.Windows.Controls;
+
+namespace Flowchart_Editor.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LinkBlockView.xaml
+    /// </summary>
+    public partial class LinkBlockView : UserControl, IBlockView
+    {
+        public LinkBlockView()
+        {
+            InitializeComponent();
+        }
+
+        public Block GetBlock(Canvas destination) => 
+            new LinkBlock(destination);
+    }
+}
